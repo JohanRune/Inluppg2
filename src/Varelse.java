78af7848113cafffc2ef271b8ffd3c83bf646dbe
JohanRune <kjgrune@yahoo.se>
2021-01-20 18:20:30 +0100

    Funkar, komplett. Trimmad

--- src/Varelse.java
+++ src/Varelse.java
@@ -27,45 +27,8 @@ public class Varelse {
         this.chef = chef;
     }
 
-
     public Varelse getChef() {
         return chef;
-    }
-
-    public void setChef(Varelse chef) {
-        this.chef = chef;
-    }
-
-    public Varelse getUnderordnad1() {
-        return underordnad1;
-    }
-
-    public void setUnderordnad1(Varelse underordnad1) {
-        this.underordnad1 = underordnad1;
-    }
-
-    public Varelse getUnderordnad2() {
-        return underordnad2;
-    }
-
-    public void setUnderordnad2(Varelse underordnad2) {
-        this.underordnad2 = underordnad2;
-    }
-
-    public Varelse getUnderordnad3() {
-        return underordnad3;
-    }
-
-    public void setUnderordnad3(Varelse underordnad3) {
-        this.underordnad3 = underordnad3;
-    }
-
-    public Varelse getUnderordnad4() {
-        return underordnad4;
-    }
-
-    public void setUnderordnad4(Varelse underordnad4) {
-        this.underordnad4 = underordnad4;
     }
 
     public String getNamn(){
