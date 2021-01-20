ea4a924faf15f7ee31f2d6cfcd4cd8bf0cb40557
JohanRune <kjgrune@yahoo.se>
2021-01-20 18:13:22 +0100

    Funkar, komplett. Man skriver in vilken tomte man vill ha lista för.

--- src/Inluppgift2.java
+++ src/Inluppgift2.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Johan Rune
@@ -26,32 +27,38 @@ public class Inluppgift2 {
     private static Varelse myran;
     private static Varelse bladlusen;
 
+    private static Scanner sc = new Scanner(System.in);
 
+    private static List<Varelse> tomtarna = new ArrayList<>();
 
     public static void main(String[] args) {
-        tomten = new Varelse("Tomten", null, glader, butter, null, null);
-        glader = new Varelse("Glader", tomten, tröger, trötter, blyger, null);
-        butter = new Varelse("Butter", tomten, rådjuret, nyckelpigan, haren, räven);
-        tröger = new Varelse("Tröger", glader);
-        trötter = new Varelse("Trötter", glader, skumtomten, null, null, null);
-        blyger = new Varelse("Blyger", glader);
-        skumtomten = new Varelse("Skumtomten", trötter, dammråttan, null, null, null);
-        dammråttan = new Varelse("Dammråttan", skumtomten);
-        rådjuret = new Varelse("Rådjuret", butter);
-        nyckelpigan = new Varelse("Nyckelpigan", butter);
-        haren = new Varelse("Haren", butter);
-        räven = new Varelse("Räven", butter, gråsuggan, myran, null, null);
-        gråsuggan = new Varelse("Gråsuggan", räven);
-        myran = new Varelse("Myran", räven, bladlusen, null, null, null);
-        bladlusen = new Varelse("Bladlusen", myran);
+        tomtarna.add(tomten = new Varelse("Tomten", null, glader, butter, null, null));
+        tomtarna.add(glader = new Varelse("Glader", tomten, tröger, trötter, blyger, null));
+        tomtarna.add(butter = new Varelse("Butter", tomten, rådjuret, nyckelpigan, haren, räven));
+        tomtarna.add(tröger = new Varelse("Tröger", glader));
+        tomtarna.add(trötter = new Varelse("Trötter", glader, skumtomten, null, null, null));
+        tomtarna.add(blyger = new Varelse("Blyger", glader));
+        tomtarna.add(skumtomten = new Varelse("Skumtomten", trötter, dammråttan, null, null, null));
+       tomtarna.add(dammråttan = new Varelse("Dammråttan", skumtomten));
+       tomtarna.add(rådjuret = new Varelse("Rådjuret", butter));
+       tomtarna.add(nyckelpigan = new Varelse("Nyckelpigan", butter));
+       tomtarna.add(haren = new Varelse("Haren", butter));
+       tomtarna.add(räven = new Varelse("Räven", butter, gråsuggan, myran, null, null));
+       tomtarna.add(gråsuggan = new Varelse("Gråsuggan", räven));
+       tomtarna.add(myran = new Varelse("Myran", räven, bladlusen, null, null, null));
+       tomtarna.add(bladlusen = new Varelse("Bladlusen", myran));
 
+        System.out.println("Vilken tomte vill du veta hierarkin för?");
+        String indata = sc.nextLine();
+        Varelse varelse = vilkenVarelse(indata);
 
         List<String> namnLista = new ArrayList();
-        namnLista= hierarki(bladlusen, namnLista);
-        for (String namn: namnLista) {
+        namnLista = hierarki(varelse, namnLista);
+        for (String namn : namnLista) {
             System.out.println(namn);
         }
     }
+
 
     //rekursiv funktion
     public static List<String> hierarki(Varelse varelse, List<String> lista) {
@@ -63,10 +70,19 @@ public class Inluppgift2 {
             lista.add(0, varelse.getChef().getNamn());
             hierarki(varelse.getChef(), lista);
 
-        }
-        catch (NullPointerException e) {
+        } catch (NullPointerException e) {
             System.out.println("Fel inträffade");
         }
         return lista;
     }
+
+    private static Varelse vilkenVarelse(String indata) {
+
+          for (int i = 0; i < tomtarna.size(); i++) {
+            if (indata.equalsIgnoreCase(tomtarna.get(i).getNamn()))
+                return tomtarna.get(i);
+        }
+        return null;
+    }
+
 }
