3314440db44581b5925e275c90569e6b1496b57c
JohanRune <kjgrune@yahoo.se>
2021-01-21 11:46:05 +0100

    Lämna in 11.45

--- src/Inluppgift2.java
+++ src/Inluppgift2.java
@@ -64,14 +64,14 @@ public class Inluppgift2 {
     public static List<String> hierarki(Varelse varelse, List<String> lista) {
 
         try {
-            if (varelse.getNamn().equals("Tomten")) {
+            if (varelse.getNamn().equals(null)) {
                 return lista;
             }
             lista.add(0, varelse.getChef().getNamn());
             hierarki(varelse.getChef(), lista);
 
         } catch (NullPointerException e) {
-            System.out.println("Fel inträffade");
+            return lista;
         }
         return lista;
     }
